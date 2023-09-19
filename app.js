ce85d33e3f79ce904fb21b9eb8249fe04126a13e
ethadnguyen <tringuyen1382003@gmail.com>
2023-09-19 20:21:45 +0700

    Dinh nghia cac route cho tai nguyen contact

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const contactRouter = require("./app/routes/contact.route");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
@@ -10,5 +10,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application" });
 });
+
+app.use("/api/contacts", contactRouter);
 
 module.exports = app;
